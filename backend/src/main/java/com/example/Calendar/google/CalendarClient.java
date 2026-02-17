64232b433b5111c753928d7cef6166fb4bfaf990
PHnsilva <PHnsilva1@gmail.com>
2026-02-17 13:28:16 -0300

    feat(integration): define CalendarClient interface

---
+++ backend/src/main/java/com/example/Calendar/google/CalendarClient.java
@@ -0,0 +1,18 @@
+package com.example.Calendar.google;
+
+import com.example.Calendar.model.Servico;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.TimePeriod;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface CalendarClient {
+    Event createEvent(Servico s) throws IOException;
+    void deleteEvent(String eventId) throws IOException;
+    Event updateEvent(Servico s) throws IOException;
+    Event getEvent(String eventId) throws IOException;
+    List<Event> listEvents(DateTime timeMin, DateTime timeMax) throws IOException;
+    List<TimePeriod> freeBusy(DateTime timeMin, DateTime timeMax) throws IOException;
+}

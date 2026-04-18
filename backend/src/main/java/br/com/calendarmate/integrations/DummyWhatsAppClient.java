e0ae57fc4230e6e97d3f9ddf79765acc7620ff10
phnsilva <phnsilva1@gmail.com>
2026-04-18 12:19:40 -0300

    chore: reduzir footprint do backend para ambiente de baixo custo

--- backend/src/main/java/br/com/calendarmate/integrations/DummyWhatsAppClient.java
+++ backend/src/main/java/br/com/calendarmate/integrations/DummyWhatsAppClient.java
@@ -1,10 +1,15 @@
 package br.com.calendarmate.integrations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DummyWhatsAppClient implements OtpDeliveryClient {
+
+    private static final Logger log = LoggerFactory.getLogger(DummyWhatsAppClient.class);
 
     @Override
     public void sendCode(String phoneDigits, String code) {
-        System.out.println("[DUMMY OTP] Enviando OTP para " + phoneDigits + ": " + code);
+        log.info("[DUMMY OTP] Enviando OTP para {}: {}", phoneDigits, code);
     }
 
     @Override
